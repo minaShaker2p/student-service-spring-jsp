400adea6a765ab80cea9da79d594e015ab917a20
Mina Rezkalla <minashakeralfy@gmail.com>
2020-07-28 12:51:04 +0200

    add login controller

---
+++ src/main/java/com/mina/studentservice/controller/LoginController.java
@@ -0,0 +1,34 @@
+package com.mina.studentservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    LoginService loginService;
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLoginPage(ModelMap model) {
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String showWelcomePage(ModelMap model, @RequestParam String name, @RequestParam String password) {
+        boolean isValidUser = loginService.validateUser(name, password);
+
+        if (!isValidUser) {
+            model.put("errorMessage", "Invlid Credentials");
+            return "login";
+        }
+
+        model.put("name", name);
+        model.put("password", password);
+        return "welcome";
+    }
+}
